b58e498c03dc62f7b3aac052d3729fd8b43e855b
SATISHKUMAR02 <133092141+SATISHKUMAR02@users.noreply.github.com>
2024-07-31 20:27:50 +0530

    Update settings.py

--- socialproject/socialproject/settings.py
+++ socialproject/socialproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9g))3zt1+cq^q4093wrjq#lzz7($r^x3ek5y6hctgw!72@o+!k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
